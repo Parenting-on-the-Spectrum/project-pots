5942f1d170a5ede843748fd9f7f804b4601b3be1
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-31 18:30:05 -0400

    fast fix

--- public/src/index.js
+++ public/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import { createRoot } from 'react-dom/client';
 
-const node = document.getElementById('root');
-const root = createRoot(node);
-root.render(<App />);
+
+// Render the App component
+ReactDOM.render(<App />, document.getElementById('root'));
