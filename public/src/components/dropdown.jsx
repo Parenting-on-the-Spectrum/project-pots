ad41f6f7827451f6f47291529d9fa589663e2b7e
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-31 09:59:59 -0400

    Add topmost title

---
+++ public/src/components/dropdown.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Typography from '@mui/material/Typography';
+
+
+const DropDown = () => {
+  return (
+    <div>
+      <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}> Dropdown List goes here </Typography>
+    </div>
+
+  )
+}
+
+export default DropDown;
---
+++ public/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Typography from '@mui/material/Typography';
+
+const Footer = (props) => {
+  return (
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}> Footer Details </Typography>
+  )
+}
+
+export default Footer;
