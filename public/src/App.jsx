e3640ff08f797740ef275e847a900b6964c69ace
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-05-31 22:36:13 -0400

    Create dropdown menu

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -4,15 +4,9 @@ import axios from 'axios';
 // jsx components go here
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
+import { Box, Typography, AppBar, Toolbar, Button, IconButton } from "@mui/material";
 // MUI components go here
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
 
 const App = () => {
   return (
--- public/src/components/header.jsx
+++ public/src/components/header.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import DropDown from './dropdown.jsx';
 // MUI components
 import { Box, Typography, AppBar, Toolbar, Button, IconButton } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = (props) => {
+
   return (
     <div>
     <Box sx={{ flexGrow: 1 }}>
@@ -16,8 +18,10 @@ const Header = (props) => {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
+
         >
-          <MenuIcon />
+          {/* <MenuIcon /> */}
+          <DropDown/>
         </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Project Pots
