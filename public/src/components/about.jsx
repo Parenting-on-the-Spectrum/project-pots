66a137176865852642d49cb99ee77cb93eee8eb7
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-06-22 22:28:49 -0400

    add photo for owner

--- public/src/components/about.jsx
+++ public/src/components/about.jsx
@@ -4,8 +4,16 @@ import { Typography } from "@mui/material";
 
 const About = (props) => {
 
+  let image = {
+    display: 'flex',
+    width: 20,
+    height: 20,
+  },
+
   return (
     <div>
+            <img src="https://i1.wp.com/handtohold.org/wp-content/uploads/2022/01/Cynthia-Shogreen-1.31.2022.png"
+      alt="Cynthia" styles={image}></img>
       <Typography variant="h2" component="div" sx={{ flexGrow: 1 }} align="center">
         Cynthia Shogreen
       </Typography>
