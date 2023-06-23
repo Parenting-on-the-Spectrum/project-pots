0acf2444738489634bdc2bbc4a9e41d5d19413de
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-06-23 14:08:10 -0400

    fix picture size issue

--- public/src/components/about.jsx
+++ public/src/components/about.jsx
@@ -1,20 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Typography } from "@mui/material";
+import Card from '@mui/material/Card';
 
 const About = (props) => {
 
-  let image = {
-    display: 'flex',
-    width: 20,
-    height: 20,
-  },
-
   return (
     <div>
-            <img src="https://i1.wp.com/handtohold.org/wp-content/uploads/2022/01/Cynthia-Shogreen-1.31.2022.png"
-      alt="Cynthia" styles={image}></img>
+
+{/* <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card> */}
+
       <Typography variant="h2" component="div" sx={{ flexGrow: 1 }} align="center">
+      <img src="https://i1.wp.com/handtohold.org/wp-content/uploads/2022/01/Cynthia-Shogreen-1.31.2022.png"
+      alt="Cynthia" width="300" height="300" align="center"></img>
         Cynthia Shogreen
       </Typography>
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="center">
@@ -22,7 +40,7 @@ const About = (props) => {
       </Typography>
 
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} padding="1%" align="center">
-        Creater
+        Creator
       </Typography>
 
     </div>
