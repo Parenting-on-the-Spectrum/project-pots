fca45af43353c6ff37daf15469275119c307b585
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-08-02 10:36:00 -0400

    add functionality for ABA providers

--- public/src/components/recComps/providers.jsx
+++ public/src/components/recComps/providers.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Typography, Modal, Box } from "@mui/material";
+import SingleProvider from './singleProvider.jsx';
 
 const Providers = (props) => {
 
   return (
-    <div>
-      Providers
+    <div >
+      {props.providers.map((i) => (
+        <SingleProvider name={i.name} link={i.link} location={i.location} key={i._id} />
+      ))}
     </div>
 
   )
---
+++ public/src/components/recComps/singleProvider.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Typography } from "@mui/material";
+import Link from '@mui/material/Link';
+
+const SingleProvider = (props) => {
+
+  return (
+    <div>
+      <Typography color="text.secondary">
+        <Link href="#" onClick={() => {
+          window.open(props.link)
+        }}>
+          {props.name} | {props.location} </Link>
+      </Typography>
+    </div>
+
+  )
+}
+
+export default SingleProvider;
