7b45bdd4b271ff861d73409d6429f50a7d6fc734
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-08-18 10:39:42 -0400

    quick fix for the localStorage

--- public/src/index.js
+++ public/src/index.js
@@ -4,7 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 
 import { createRoot } from 'react-dom/client';
 let firstTheme = localStorage.getItem('theme')
-if (firstTheme === undefined) {localStorage.setItem('theme', 'light')}
+if (firstTheme === undefined || firstTheme === null) {
+  localStorage.setItem('theme', 'light');
+}
 
 const node = document.getElementById('root');
 const root = createRoot(node);
