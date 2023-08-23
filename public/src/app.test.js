6ded947b9bdd039685f6b1f5acde7dccc4a9c4e6
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-08-23 09:41:14 -0400

    add tests and dependancies, but I'm still waiting until the next PR to add new deps

--- public/src/app.test.js
+++ public/src/app.test.js
@@ -1,5 +1,43 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen, getByText } from '@testing-library/react';
+// import '@testing-library/jest-dom';
+import Mission from './components/mission.jsx';
+
 describe("Example tests", function () {
+
   test('sample test', () => {
     expect(true).toEqual(true);
   });
+
+  test('use jsdom in this test file', () => {
+    const element = document.createElement('div');
+    expect(element).not.toBeNull();
+  });
+
+});
+
+describe("Page tests", function () {
+
+  test("Mission is a function", () => {
+    expect(typeof Mission).toBe('function')
+  });
+
+  test('Mission renders', () => {
+    /*
+    render(<Mission />);
+    console.log(screen.getByText(/complexities/))
+
+    expect(screen.getByText(/complexities/).includes()).toEqual(true);
+    expect(screen.getByText(/complexities/)).toBeInTheDocument();
+    npm i --save-dev @testing-library/jest-dom
+    */
+
+   let texts = Mission().props.children[0].props.children;
+   expect(texts.includes('Parenting')).toEqual(true);
+  });
+
 });
