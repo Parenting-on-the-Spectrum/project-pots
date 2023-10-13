89a38409f54cfb580a97973e1528a62813c5c357
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-10-13 09:11:11 -0400

    add the extra onhover functionality

--- public/src/components/vidComps/singleKidVid.jsx
+++ public/src/components/vidComps/singleKidVid.jsx
@@ -8,9 +8,23 @@ import CardContent from '@mui/material/CardContent';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const SingleKidVid = (props) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  useEffect(() => {
+    Card
+  }, [isHovering])
+
+  const styler = () => {
+    if (isHovering) {
+      return {
+        opacity: 0.5
+      }
+    }
+  }
+
   const card = (
     <React.Fragment>
-      <center><img src={props.thumbnails.medium.url} width='100%' sx={{justifyContent: "center"}} alt={props.title}></img></center>
+      <center><img src={props.thumbnails.medium.url} width='100%' sx={{ justifyContent: "center" }} alt={props.title}></img></center>
       <CardContent>
         <Typography variant="h5" component="div">
           {props.title}
@@ -21,14 +35,16 @@ const SingleKidVid = (props) => {
       </CardContent>
       <CardActions>
         <Link href={`https://www.youtube.com/watch?v=${props.id}`} target="_blank" rel="noopener">
-        <YouTubeIcon/>
+          <YouTubeIcon style={styler()}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)} />
         </Link>
       </CardActions>
     </React.Fragment>
   );
 
   return (
-    <Box sx={{ width: '300px', padding: '2%'}}>
+    <Box sx={{ width: '300px', padding: '2%' }}>
       <Card variant="outlined" >{card}</Card>
     </Box>
   )
