ccb1813d5ed7d894a16023552da74edb68aa74bc
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-10-23 09:19:15 -0400

    add more to the caregiver slice; complete all the vi buttons

--- public/src/components/vidComps/caregiverVideos.jsx
+++ public/src/components/vidComps/caregiverVideos.jsx
@@ -3,9 +3,11 @@ import axios from 'axios';
 import SingleCareVid from './singleCareVid.jsx';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const CareGiverVideos = (props) => {
   const [careVids, setCareVids] = useState([]);
+  const [slice, setSlice] = useState(4)
   var counter = 0;
 
   let careGivers = () => {
@@ -16,8 +18,25 @@ const CareGiverVideos = (props) => {
       .catch((err) => err.stack)
   }
 
+  const show = () => {
+    if (slice < careVids.length) {
+      return (
+        <center>
+          <Button variant="contained" onClick={(e) => {
+            e.preventDefault();
+            setSlice(slice + 4)
+          }}>
+            More Videos
+          </Button>
+        </center>
+      )
+    }
+    else { return; }
+  }
+
   useEffect(() => {
     careGivers();
+    show();
   }, []);
 
   return (
@@ -26,13 +45,15 @@ const CareGiverVideos = (props) => {
         <CircularProgress />
       </Box>
       :
+      <div>
       <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        {careVids.map((k) => (
+        {careVids.slice(0, slice).map((k) => (
           <SingleCareVid thumbnails={k.snippet.thumbnails} description={k.snippet.description}
             title={k.snippet.title} key={counter++} id={k.id.videoId} />
         ))}
       </Box>
-
+      {show()}
+      </div>
   )
 }
 
